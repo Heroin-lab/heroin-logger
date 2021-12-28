32f4c55e12205640c9c7b14c19501f8d80ae78f6
Heroin-lab <vip.serkin@gmail.com>
2021-12-28 02:12:07 +0200

    add new debug function and create log level changer

--- logger.go
+++ logger.go
@@ -2,30 +2,66 @@ package logger
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
+
+var logLevel = "info"
+
+func SetLogLevel(lvl string) error {
+	switch strings.ToLower(lvl) {
+	case "info":
+		logLevel = "info"
+		return nil
+	case "debug":
+		logLevel = "debug"
+		return nil
+	case "error", "err":
+		logLevel = "error"
+		return nil
+	case "warning", "warn":
+		logLevel = "warning"
+		return nil
+	}
+
+	Error("Not valid log level [", lvl, "] please, set the correct level. "+
+		"\nAvalible levels: \n 'info' \n 'debug' \n 'warning \n 'error'")
+
+	return fmt.Errorf("Not valid log level %d", lvl)
+}
 
 func getTime() string {
 	fullTime := time.Now().Format("01-02-2006 15:04:05")
 	return fullTime
 }
 
-func Info(message string, params interface{}) {
-	currentTime := getTime()
-	fmt.Println(currentTime, "\u001B[36m[INFO]\u001B[0m", message, " params: ", params)
+func DebugMsg(message ...interface{}) {
+	if logLevel == "debug" {
+		currentTime := getTime()
+		fmt.Printf("\n [%s] \033[32m[DEBUG]\u001B[0m lvl=%s  msg= %s", currentTime, logLevel, message)
+	}
 }
 
-func Error(message string, params interface{}) {
-	currentTime := getTime()
-	fmt.Println(currentTime, "\033[31m[ERROR]\u001B[0m", message, " params: ", params)
+func Info(message ...interface{}) {
+	if logLevel == "info" || logLevel == "debug" {
+		currentTime := getTime()
+		fmt.Printf("\n [%s] \u001B[36m[INFO]\u001B[0m lvl=%s  msg= %s", currentTime, logLevel, message)
+	}
 }
 
-func Warning(message string, params interface{}) {
+func Error(message ...interface{}) {
 	currentTime := getTime()
-	fmt.Println(currentTime, "\033[33m[WARNING]\u001B[0m", message, " params: ", params)
+	fmt.Printf("\n [%s] \033[31m[ERROR]\u001B[0m lvl=%s  msg= %s", currentTime, logLevel, message)
 }
 
-func Fatal(message string, params interface{}) {
+func Warning(message ...interface{}) {
+	if logLevel != "error" {
+		currentTime := getTime()
+		fmt.Printf("\n [%s] \033[33m[WARNING]\u001B[0m lvl= %s  msg= %s", currentTime, logLevel, message)
+	}
+}
+
+func Fatal(message ...interface{}) {
 	currentTime := getTime()
-	fmt.Println(currentTime, "\033[31m[FATAL]\u001B[0m", message, " params: ", params)
+	fmt.Printf("\n [%s] \033[31m[FATAL]\u001B[0m lvl= %sa  msg= %s", currentTime, logLevel, message)
 }
